d235ba7ca9f05ac63e57a86a97e8be75a6ef69d2
Andre G. S. Paschoa <108034536+andregspaschoa@users.noreply.github.com>
2023-03-17 23:19:12 -0300

    Create a Login Page
    Create a Login Page 1.0v

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,29 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {InputTextModule} from 'primeng/inputtext';
+import {PasswordModule} from 'primeng/password';
 import {CardModule} from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
+import {ToolbarModule} from 'primeng/toolbar';
+import {SplitButtonModule} from 'primeng/splitbutton';
+import { LoginComponent } from './views/login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     CardModule,
     ButtonModule,
+    ToolbarModule,
+    SplitButtonModule,
+    InputTextModule,
+    PasswordModule,
     AppRoutingModule
   ],
   providers: [],
